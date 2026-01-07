a0bd1dfffb787052e08b8e642a02ebb7572308c8
Lakhan5646 <plakhan360@gmail.com>
2026-01-07 17:40:11 +0530

    Add files via upload

---
+++ app/app.py
@@ -0,0 +1,34 @@
+
+import streamlit as st
+import numpy as np
+import joblib
+import os
+st.write(os.getcwd())
+
+model = joblib.load("C:/Users/hp/Desktop/Minor/models/best_diabetes_model.pkl")
+
+st.title("🩺 Diabetes Risk Prediction App")
+st.write("Enter patient details to predict diabetes risk")
+
+pregnancies = st.number_input("Pregnancies", 0, 20, 1)
+glucose = st.number_input("Glucose Level", 0, 300, 120)
+blood_pressure = st.number_input("Blood Pressure", 0, 200, 70)
+skin_thickness = st.number_input("Skin Thickness", 0, 100, 20)
+insulin = st.number_input("Insulin", 0, 900, 80)
+bmi = st.number_input("BMI", 0.0, 70.0, 25.0)
+diabetes_pedigree = st.number_input("Diabetes Pedigree Function", 0.0, 3.0, 0.5)
+age = st.number_input("Age", 1, 120, 30)
+
+if st.button("Predict Diabetes Risk"):
+    input_data = np.array([[pregnancies, glucose, blood_pressure,
+                             skin_thickness, insulin, bmi,
+                             diabetes_pedigree, age]])
+
+    probability = model.predict_proba(input_data)[0][1]
+
+    if probability < 0.4:
+        st.success(f"Low Risk of Diabetes (Score: {probability:.2f})")
+    elif probability < 0.7:
+        st.warning(f"Medium Risk of Diabetes (Score: {probability:.2f})")
+    else:
+        st.error(f"High Risk of Diabetes (Score: {probability:.2f})")
